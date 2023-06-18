ab5a441b5ac87687cc3db034db555e80f713ee56
sup12123 <95585537+sup12123@users.noreply.github.com>
2023-06-18 19:24:35 +0000

    Update DB-query.c

--- DB-query.c
+++ DB-query.c
@@ -1,121 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <gtk/gtk.h>
 #include <libpq-fe.h>
 
-GtkWidget *entry1;
-GtkWidget *entry2;
-GtkWidget *entry3;
-GtkWidget *entry4;
-GtkWidget *status_label;
 PGconn *conn = NULL;
 
-void connect_database(GtkWidget *widget, gpointer data) {
-    const gchar *dbname = gtk_entry_get_text(GTK_ENTRY(entry1));
-    const gchar *username = gtk_entry_get_text(GTK_ENTRY(entry2));
-    const gchar *password = gtk_entry_get_text(GTK_ENTRY(entry3));
-    const gchar *host = gtk_entry_get_text(GTK_ENTRY(entry4));
+void connect_database() {
+    const char *params[] = {
+        "dbname=customer_profiles",
+        NULL
+    };
 
-    // Construct the connection string
-    gchar *conninfo = g_strdup_printf("dbname=%s user=%s password=%s host=%s", dbname, username, password, host);
-    
-    // Connect to the database
-    conn = PQconnectdb(conninfo);
-    
-    // Check if the connection was successful
+    conn = PQconnectdbParams(params, 1); // Set expand_dbname flag to 1
+
     if (PQstatus(conn) != CONNECTION_OK) {
-        gchar *error_message = g_strdup_printf("Connection to database failed: %s", PQerrorMessage(conn));
-        gtk_label_set_text(GTK_LABEL(status_label), error_message);
-        g_free(error_message);
+        fprintf(stderr, "Connection to database failed: %s", PQerrorMessage(conn));
         PQfinish(conn);
-        conn = NULL;
-        g_free(conninfo);
-        return;
+        exit(1);
     }
-    
-    gtk_label_set_text(GTK_LABEL(status_label), "Connected to database");
-    
-    g_free(conninfo);
 }
 
 void add_entry(GtkWidget *widget, gpointer data) {
-    if (!conn) {
-        gtk_label_set_text(GTK_LABEL(status_label), "Not connected to database");
-        return;
-    }
-    
     const gchar *name = gtk_entry_get_text(GTK_ENTRY(entry1));
-    const gchar *email = gtk_entry_get_text(GTK_ENTRY(entry2));
-    const gchar *phone = gtk_entry_get_text(GTK_ENTRY(entry3));
+    const gchar *age = gtk_entry_get_text(GTK_ENTRY(entry2));
+    const gchar *email = gtk_entry_get_text(GTK_ENTRY(entry3));
     const gchar *weight = gtk_entry_get_text(GTK_ENTRY(entry4));
     const gchar *origin = gtk_entry_get_text(GTK_ENTRY(entry5));
     const gchar *destination = gtk_entry_get_text(GTK_ENTRY(entry6));
-    
-    // Begin a database transaction
-    PGresult *res = PQexec(conn, "BEGIN");
-    
-    // Check if the transaction was successful
+
+    char query[256];
+    sprintf(query, "INSERT INTO profiles (name, age, email, weight, origin, destination) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')",
+            name, age, email, weight, origin, destination);
+
+    PGresult *res = PQexec(conn, query);
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        gchar *error_message = g_strdup_printf("Failed to begin database transaction: %s", PQerrorMessage(conn));
-        gtk_label_set_text(GTK_LABEL(status_label), error_message);
-        g_free(error_message);
+        fprintf(stderr, "Failed to execute query: %s", PQerrorMessage(conn));
         PQclear(res);
         return;
     }
-    
-    // Prepare the SQL statements
-    gchar *sql1 = g_strdup_printf("INSERT INTO customer_profile (name, email, phone) VALUES ('%s', '%s', '%s') RETURNING id", name, email, phone);
-    gchar *sql2 = g_strdup_printf("INSERT INTO bags (weight, origin, destination, profile_id) VALUES (%s, '%s', '%s', $1)", weight, origin, destination);
-    
-    // Execute the first SQL statement and get the profile_id
-    res = PQexecParams(conn, sql1, 0, NULL, NULL, NULL, NULL, 1);
-    
-    // Check if the execution was successful
-    if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-        gchar *error_message = g_strdup_printf("Failed to execute SQL statement: %s", PQerrorMessage(conn));
-        gtk_label_set_text(GTK_LABEL(status_label), error_message);
-        g_free(error_message);
-        PQclear(res);
-        g_free(sql1);
-        g_free(sql2);
-        return;
-    }
-    
-    // Get the profile_id from the result
-    const gchar *profile_id = PQgetvalue(res, 0, 0);
-    
-    // Execute the second SQL statement with the retrieved profile_id
-    const char *params[1] = { profile_id };
-    res = PQexecParams(conn, sql2, 1, NULL, params, NULL, NULL, 0);
-    
-    // Check if the execution was successful
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        gchar *error_message = g_strdup_printf("Failed to execute SQL statement: %s", PQerrorMessage(conn));
-        gtk_label_set_text(GTK_LABEL(status_label), error_message);
-        g_free(error_message);
-        PQclear(res);
-        g_free(sql1);
-        g_free(sql2);
-        return;
-    }
-    
-    // Commit the database transaction
-    res = PQexec(conn, "COMMIT");
-    
-    // Check if the transaction was successful
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        gchar *error_message = g_strdup_printf("Failed to commit database transaction: %s", PQerrorMessage(conn));
-        gtk_label_set_text(GTK_LABEL(status_label), error_message);
-        g_free(error_message);
-        PQclear(res);
-        g_free(sql1);
-        g_free(sql2);
-        return;
-    }
-    
-    gtk_label_set_text(GTK_LABEL(status_label), "Entry added to the database");
-    
+
     PQclear(res);
-    g_free(sql1);
-    g_free(sql2);
+
+    gtk_label_set_text(GTK_LABEL(status_label), "Entry added successfully");
 }
 
 int main(int argc, char *argv[]) {
@@ -133,33 +59,33 @@ int main(int argc, char *argv[]) {
     // Initialize GTK
     gtk_init(&argc, &argv);
 
+    // Connect to the database
+    connect_database();
+
     // Create the main window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Customer Profile");
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+    gtk_window_set_title(GTK_WINDOW(window), "Customer Profile Management");
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // Create a vertical box
-    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    // Create a vertical box container
+    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
     // Create a grid
     grid = gtk_grid_new();
-    gtk_grid_set_column_spacing(GTK_GRID(grid), 5);
     gtk_grid_set_row_spacing(GTK_GRID(grid), 5);
-    gtk_box_pack_start(GTK_BOX(vbox), grid, FALSE, FALSE, 0);
+    gtk_grid_set_column_spacing(GTK_GRID(grid), 10);
+    gtk_box_pack_start(GTK_BOX(vbox), grid, TRUE, TRUE, 0);
 
     // Create labels
     label1 = gtk_label_new("Name:");
-    label2 = gtk_label_new("Email:");
-    label3 = gtk_label_new("Phone:");
+    label2 = gtk_label_new("Age:");
+    label3 = gtk_label_new("Email:");
     label4 = gtk_label_new("Weight (kg):");
-    label5 = gtk_label_new("Origin:");
-    label6 = gtk_label_new("Destination:");
+    label5 = gtk_label_new("Origin (IATA):");
+    label6 = gtk_label_new("Destination (IATA):");
 
-    // Add labels to the grid
+    // Attach labels to the grid
     gtk_grid_attach(GTK_GRID(grid), label1, 0, 0, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), label2, 0, 1, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), label3, 0, 2, 1, 1);
@@ -175,7 +101,7 @@ int main(int argc, char *argv[]) {
     entry5 = gtk_entry_new();
     entry6 = gtk_entry_new();
 
-    // Add entry fields to the grid
+    // Attach entry fields to the grid
     gtk_grid_attach(GTK_GRID(grid), entry1, 1, 0, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), entry2, 1, 1, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), entry3, 1, 2, 1, 1);
@@ -183,27 +109,23 @@ int main(int argc, char *argv[]) {
     gtk_grid_attach(GTK_GRID(grid), entry5, 1, 4, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), entry6, 1, 5, 1, 1);
 
-    // Create a button
-    add_button = gtk_button_new_with_label("Add Entry");
-    gtk_box_pack_start(GTK_BOX(vbox), add_button, FALSE, FALSE, 0);
+    // Create the Add button
+    add_button = gtk_button_new_with_label("Add");
+    g_signal_connect(add_button, "clicked", G_CALLBACK(add_entry), NULL);
+    gtk_grid_attach(GTK_GRID(grid), add_button, 1, 6, 1, 1);
 
-    // Create a status label
+    // Create the status label
     status_label = gtk_label_new("");
     gtk_box_pack_start(GTK_BOX(vbox), status_label, FALSE, FALSE, 0);
-
-    // Connect signals
-    g_signal_connect(add_button, "clicked", G_CALLBACK(add_entry), NULL);
 
     // Show all widgets
     gtk_widget_show_all(window);
 
-    // Run the GTK main loop
+    // Start the GTK main loop
     gtk_main();
 
-    // Cleanup
-    if (conn) {
-        PQfinish(conn);
-    }
+    // Cleanup and exit
+    PQfinish(conn);
 
     return 0;
 }
